5575c551fda7424094ca0e65fdf14b1429101484
kimkihyun98 <132446330+kimkihyun98@users.noreply.github.com>
2023-05-31 15:02:31 +0900

    Update MypageController.java

--- src/main/java/com/reservation/controller/MypageController.java
+++ src/main/java/com/reservation/controller/MypageController.java
@@ -3,6 +3,6 @@ package com.reservation.controller;
 import org.springframework.stereotype.Controller;
 
 @Controller
-@RequestMapping("/mypage")
+@RequestMapping(value ="/mypage")
 public class MypageController {
 }
