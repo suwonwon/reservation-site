4cc793b149e2e44316f3f9415f46530304d35800
kimkihyun98 <132446330+kimkihyun98@users.noreply.github.com>
2023-05-31 14:16:27 +0900

    Update Comment.java

--- src/main/java/com/reservation/dto/Comment.java
+++ src/main/java/com/reservation/dto/Comment.java
@@ -11,12 +11,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Table;
 import java.util.Date;
-@Entity
+
 @Data
-@Table(name = "comment")
 public class Comment {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+ 
     private int c_id;
     private int m_id;
     private int p_id;
