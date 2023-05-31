f4dba3040a7e84cfa1608180c0cd0591a3d8f881
kimkihyun98 <132446330+kimkihyun98@users.noreply.github.com>
2023-05-31 14:18:04 +0900

    Update Star.java

--- src/main/java/com/reservation/dto/Star.java
+++ src/main/java/com/reservation/dto/Star.java
@@ -8,12 +8,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Table;
 
-@Entity
+
 @Data
-@Table(name = "star")
 public class Star {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private int s_id;
     private int m_id;
     private int rs_id;
