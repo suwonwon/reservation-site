32b47d3504fff60090f055f4af1c2ec3a5ea8181
kimkihyun98 <132446330+kimkihyun98@users.noreply.github.com>
2023-05-31 14:17:41 +0900

    Update Review.java

--- src/main/java/com/reservation/dto/Review.java
+++ src/main/java/com/reservation/dto/Review.java
@@ -8,12 +8,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Table;
 
-@Entity
+
 @Data
-@Table(name = "review")
 public class Review {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private int r_id;
     private int rs_id;
     private int m_id;
