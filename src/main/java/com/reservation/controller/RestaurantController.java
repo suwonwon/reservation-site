4b1f6b300b20ffa13bb9e0de918f8a17450878ad
kimkihyun98 <132446330+kimkihyun98@users.noreply.github.com>
2023-05-31 15:02:43 +0900

    Update RestaurantController.java

--- src/main/java/com/reservation/controller/RestaurantController.java
+++ src/main/java/com/reservation/controller/RestaurantController.java
@@ -4,6 +4,6 @@ package com.reservation.controller;
 import org.springframework.stereotype.Controller;
 
 @Controller
-@RequestMapping("/restaurant")
+@RequestMapping(value ="/restaurant")
 public class RestaurantController {
 }
