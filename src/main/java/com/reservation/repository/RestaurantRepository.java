bd4c13e75af62ccf05c8c029501f82286cf6a026
kimkihyun98 <132446330+kimkihyun98@users.noreply.github.com>
2023-05-31 14:30:01 +0900

    Update RestaurantRepository.java

--- src/main/java/com/reservation/repository/RestaurantRepository.java
+++ src/main/java/com/reservation/repository/RestaurantRepository.java
@@ -3,5 +3,5 @@ package com.reservation.repository;
 import com.reservation.entity.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+public interface RestaurantRepository extends JpaRepository<Restaurant, Intger> {
 }
