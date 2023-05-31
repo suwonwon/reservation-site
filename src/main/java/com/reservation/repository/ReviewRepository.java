936795bc2d4d5f81456db75676019190cd1a2657
kimkihyun98 <132446330+kimkihyun98@users.noreply.github.com>
2023-05-31 14:30:09 +0900

    Update ReviewRepository.java

--- src/main/java/com/reservation/repository/ReviewRepository.java
+++ src/main/java/com/reservation/repository/ReviewRepository.java
@@ -3,5 +3,5 @@ package com.reservation.repository;
 import com.reservation.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReviewRepository extends JpaRepository<Review, Long> {
+public interface ReviewRepository extends JpaRepository<Review, Intger> {
 }
