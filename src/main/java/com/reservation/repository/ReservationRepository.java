740090835e9c55dbb0857268be580a8f8595bf24
kimkihyun98 <132446330+kimkihyun98@users.noreply.github.com>
2023-05-31 15:04:43 +0900

    Update ReservationRepository.java

--- src/main/java/com/reservation/repository/ReservationRepository.java
+++ src/main/java/com/reservation/repository/ReservationRepository.java
@@ -1,6 +1,6 @@
 package com.reservation.repository;
 
-import com.reservation.dto.Reservation;
+import com.reservation.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
