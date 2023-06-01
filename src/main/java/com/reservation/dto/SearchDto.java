981ab2a28ae5dbf5cb8c85f49b09e5d52c86f109
yuheesong <thddbgml345@naver.com>
2023-06-01 19:24:55 +0900

    기타 세팅

--- src/main/java/com/reservation/dto/SearchDto.java
+++ src/main/java/com/reservation/dto/SearchDto.java
@@ -1,5 +1,7 @@
 package com.reservation.dto;
 
+import lombok.Data;
+
 @Data
 public class SearchDto {
 }
--- src/main/java/com/reservation/dto/UserFormDto.java
+++ src/main/java/com/reservation/dto/UserFormDto.java
@@ -1,4 +1,7 @@
 package com.reservation.dto;
+
+import lombok.Data;
+
 //회원가입 폼 - 일반고객
 @Data
 public class UserFormDto {
